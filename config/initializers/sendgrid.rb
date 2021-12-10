d1fe27684e9899f0b5b903309d6c7428dc48c10a
Christa Hartsock <christa.hartsock@gmail.com>
2021-12-09 21:09:42 -0800

    Remove unused sendgrid configuration

--- config/initializers/sendgrid.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  domain: ENV["SMTP_DOMAIN"],
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
