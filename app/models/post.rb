be750d6fb33dd25dc7b6e676d1b283d4d714c66e
Christa Hartsock <christa.hartsock@gmail.com>
2021-12-09 22:46:01 -0800

    Don't require posts to have topic when they are built

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :topic
+    belongs_to :topic, optional: true
     belongs_to :author, class_name: 'User', inverse_of: :posts
 
     validates_presence_of :body, :author
