8f0e40028a2e6668bb32c3d7e544b7794b9c106d
Christa Hartsock <christa.hartsock@gmail.com>
2021-12-12 09:41:57 -0800

    Fix credential calling in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -54,12 +54,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: "https://#{Rails.application.credentials.domain}", from: "bot@#{Rails.application.credentials[:address]}" }
+  config.action_mailer.default_url_options = { host: "https://#{Rails.application.credentials.domain}", from: "bot@#{Rails.application.credentials.smtp[:address]}" }
   config.action_mailer.smtp_settings = {
     authentication: :plain,
     address: Rails.application.credentials.smtp[:hostname],
     port: 587,
-    domain: Rails.application.credentials[:address],
+    domain: Rails.application.credentials.smtp[:address],
     user_name: Rails.application.credentials.smtp[:user_name],
     password: Rails.application.credentials.smtp[:password],
   }
