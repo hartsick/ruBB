007d9a838a8e6f23159075171e79ff52a5ca4e27
Christa Hartsock <christa.hartsock@gmail.com>
2021-12-12 16:15:53 -0800

    Give devise an email fallback in env without credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,7 +16,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV.fetch("EMAIL_FROM_ADDRESS", "bot@#{Rails.application.credentials.smtp[:address]}")
+  config.mailer_sender = ENV.fetch("EMAIL_FROM_ADDRESS", "bot@#{Rails.application.credentials.dig(:smtp, :address) || 'example.com'}")
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
