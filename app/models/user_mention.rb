31c8dfab2869b031de0971cee4fe89f2d523850f
Christa Hartsock <christa.hartsock@gmail.com>
2022-01-06 18:27:19 -0800

    Don't cache username regex across requests
    Was preventing new users from receiving notifications

--- app/models/user_mention.rb
+++ app/models/user_mention.rb
@@ -3,13 +3,10 @@ class UserMention < ApplicationRecord
     belongs_to :post
 
     CAPTURE_USERNAME_REGEX = /@(\w+)/
-    @@format_username_regex = ''
 
     def self.format_username_regex
-        return @@format_username_regex if @@format_username_regex.present?
-
         all_usernames = User.pluck(:username).map{|username| Regexp.escape("@#{username}")}
-        @@format_username_regex = Regexp.union(all_usernames)
+        @format_username_regex = Regexp.union(all_usernames)
     end
 
     def self.associate_mentions_in_post!(post)
@@ -18,7 +15,7 @@ class UserMention < ApplicationRecord
 
         user_mentions = User.where(username: username_mentions)
         return unless user_mentions.any?
-        
+
         now = Time.now
         insert_all!(user_mentions.map{|user| { user_id: user.id, post_id: post.id, created_at: now, updated_at: now } })
     end
