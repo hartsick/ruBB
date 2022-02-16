473981c1f9d73b34d52765898e7a7ad2d1bdceb8
Christa Hartsock <christa.hartsock@gmail.com>
2022-02-16 08:26:52 -0800

    Fully move to Rails 7 config

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,8 @@ Bundler.require(*Rails.groups)
 
 module Forum
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    # Initialize configuration defaults
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
