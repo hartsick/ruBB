10ff897a6061c7900c0290a3d5776680be8b713f
Christa Hartsock <christa.hartsock@gmail.com>
2022-03-30 08:27:15 -0700

    Disable Turbo caching on main page
    Turbo caching page meant that users often saw outdated read vs unread
    thread styling on the main page (since we're not controlling this in
    Javascript). For now, I'm disabling the caching, which will slow down
    load times for users. In the longterm, I'd like to move toward updating
    state before page cache using Turbo hooks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def title(text)
         content_for :title, text
     end
+
+    def disable_cache
+        content_for :cache_control, '<meta name="turbo-cache-control" content="no-cache">'.html_safe
+    end
 end
