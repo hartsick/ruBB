a3459606014c1dd08734b1af1d0064644618a559
Christa Hartsock <christa.hartsock@gmail.com>
2024-04-19 10:37:41 -0700

    Summer theme!

--- app/models/site_theme.rb
+++ app/models/site_theme.rb
@@ -1,10 +1,26 @@
 class SiteTheme
     def self.pick_color(number)
         color_wheel = %w[
-            bg-amber-500
-            bg-amber-400
+            bg-amber-100
+            bg-amber-200
             bg-amber-300
             bg-amber-200
+            bg-amber-100
+            bg-rose-100
+            bg-rose-200
+            bg-rose-300
+            bg-rose-200
+            bg-rose-100
+            bg-cyan-100
+            bg-cyan-200
+            bg-cyan-300
+            bg-cyan-200
+            bg-cyan-100
+            bg-rose-100
+            bg-rose-200
+            bg-rose-300
+            bg-rose-200
+            bg-rose-100
         ]
 
         index = number % color_wheel.length
